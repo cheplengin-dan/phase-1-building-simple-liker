a13a8a3edaa9f26f37b6aad8a86a428707a8b157
Duncan <cheplenginduncan@gmail.com>
2023-04-04 08:07:17 +0300

    Completed Assignments

--- main.js
+++ main.js
@@ -1,9 +1,33 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const modal = document.querySelector('#modal');
+const modalMessage = document.querySelector('#modal-message');
 
 // Your JavaScript code goes here!
+document.addEventListener('click', function(event) {
+  if (event.target.classList.contains('like-glyph')) {
+    const heart = event.target;
 
+    if (heart.innerText === EMPTY_HEART) {
+      mimicServerCall()
+        .then(function(serverMessage) {
+          heart.innerText = FULL_HEART;
+          heart.classList.add('activated-heart');
+        })
+        .catch(function(errorMessage) {
+          modalMessage.innerText = errorMessage;
+          modal.classList.remove('hidden');
+          setTimeout(function() {
+            modal.classList.add('hidden');
+          }, 3000);
+        });
+    } else {
+      heart.innerText = EMPTY_HEART;
+      heart.classList.remove('activated-heart');
+    }
+  }
+});
 
 
 
